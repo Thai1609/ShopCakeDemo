719217faf5fd1114d63a274cf62debbd06d56729
Thai1609 <nguyenxuanthai1609@gmail.com>
2022-11-25 15:36:51 +0700

    michael.nguyen
    first commit 25/11/2022

---
+++ src/main/java/com/dev/ShopCake/ShopCakeApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ShopCake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCakeApplication.class, args);
+	}
+
+}
